ddfa3af4f723a6e3b8fa4cb68a8ff20dc1498442
Luming-L <55735395+Luming-L@users.noreply.github.com>
2020-06-16 21:27:19 +0100

    run peakOverlap.py on eddie

---
+++ Results/MotifFind/peakOverlap_batch.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -cwd -V                  
+#$ -l h_rt=00:05:00
+#$ -l h_vmem=2G
+ 
+#$ -t 257-257
+ 
+# Configure modules
+. /etc/profile.d/modules.sh
+module load igmm/apps/BEDTools/2.27.1
+module load python/3.4.3
+
+# # prepare the input of bedtools
+# echo "sort start: $(date)"
+# # sort and cut files of Cancer Type-specific PeakCalls
+# cd /exports/eddie/scratch/s1949868/TCGA-ATAC_Cancer_Type-specific_PeakCalls
+# for file in $(ls); do sort -k1,1 -k2,2n $file | awk '{FS=OFS="\t"; if($1~/^chr/){print $1,$2,$3,$4;}}' > /exports/eddie/scratch/s1949868/Sample_PeakCalls/${file}.sorted; done
+# # cut files of recalled peaks in each technical replicate
+# cd /exports/eddie/scratch/s1949868/PeakRecall_peaks001
+# for file in $(ls); do sort -k1,1 -k2,2n $file | awk '{FS=OFS="\t"; if($1~/^chr/){print $1,$2,$3}}' > /exports/eddie/scratch/s1949868/Sample_PeakCalls/${file}.sorted; done
+# echo "sort done: $(date)"
+
+# peakOverlap
+echo "peakOverlap start: $(date)"
+python3 /home/s1949868/peakOverlap.py $SGE_TASK_ID
+echo "peakOverlap done: $(date)"
