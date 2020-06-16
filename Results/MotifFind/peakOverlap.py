db14c8afed2dd574b265cce1c26831deffa631d8
Luming-L <55735395+Luming-L@users.noreply.github.com>
2020-06-16 21:24:14 +0100

    Final version of peakOverlap.py

--- Results/MotifFind/peakOverlap.py
+++ Results/MotifFind/peakOverlap.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import os, sys
 import subprocess as sp
 
-# number of cancer type
+# number of a biological sample
 num = int(sys.argv[1])
 print(num)
 
@@ -12,41 +12,40 @@ Cancer_Type_PeakCalls_path = "/exports/eddie/scratch/s1949868/TCGA-ATAC_Cancer_T
 peaks001_path = "/exports/eddie/scratch/s1949868/PeakRecall_peaks001"
 Sample_PeakCalls_path = "/exports/eddie/scratch/s1949868/Sample_PeakCalls"
 
-# get abbreviations of 23 cancer types
-Cancer_types = [i.split("_")[0] for i in os.listdir(Cancer_Type_PeakCalls_path)]
-print(Cancer_types)
+# use the same ID to identify technical replicates from the same biological sample
+l1 = ["_".join(i.split("_")[0:8]) for i in os.listdir(Sample_PeakCalls_path) if i.endswith(".bed.sorted")]
+print(len(l1))
 
-# function
-def peakOverlap(Cancer_type, path):
+# count technical replicates for each ID and store them in a dict
+bioSample_dict={}
+for item in set(l1):
+	bioSample_dict[item]=l1.count(item)
+print(list(bioSample_dict.keys())[-1])
+print(sorted(list(set(l1)))[-1])
+
+
+# peakOverlap() function
+def peakOverlap(sample, path):
 	'''
-	Obtain PeakCalls of 404 biological samples by using `bedtools intersect`:
-	for each technical replicate(796), choose recalled peaks overlapping with corresponding cancer type-specific peakCalls
-	only report peaks that are overlapped 100%
-	peakCalls in two replicates from the same sample will be merged
+	Refine recalled peaks from 796 technical replicates by using `bedtools intersect`:
+	1.for each technical replicate, output cancer type-specific peaks that can be 100% overlapped by recalled peaks
+	2.refined peaks in two replicates from the same sample will be merged. only report peaks obeserved in two replicates
+	3.finally get a list of peaks for each of 410 biological samples
 	Args:
-		Cancer_type: the abbreviation of a cancer type
+		sample: ID for replicates from the same biological sample
 		path: the path of input files (sorted Cancer type-specific peakCalls and all samples recalled peaks) and output files
 	'''
-	# use the same ID to identify technical replicates from the same biological sample
-	l1 = ["_".join(i.split("_")[0:8]) for i in os.listdir(path) if i.startswith(Cancer_type) and i.endswith(".bed.sorted")]
-	# count technical replicates for each ID and store them in a dict
-	bioSample_dict={}
-	for item in l1:
-		if item in bioSample_dict:
-			bioSample_dict[item]+=1
-		else:
-			bioSample_dict[item]=1
-	print(bioSample_dict)
-	for sample in bioSample_dict.keys():
-		if bioSample_dict[sample] == 1:
-			os.system("bedtools intersect -a " + path + "/" + Cancer_type + "*txt.sorted -b " + path + "/" + sample + "* -f 1.0 -wa -sorted > " + path + "/" + sample+"_peakCalls.bed")
-			print("bedtools intersect -a " + path + "/" + Cancer_type + "*txt.sorted -b " + path + "/" + sample + "* -f 1.0 -wa -sorted > " + path + "/" + sample+"_peakCalls.bed")
-		elif bioSample_dict[sample] == 2:
-			os.system("bedtools intersect -a " + path + "/" + Cancer_type + "*txt.sorted -b " + path + "/" + sample + "* -f 1.0 -c -wa -sorted " + " | awk '{FS=OFS=" + r'"\t"' + ";if($5==2){print $1,$2,$3,$4}}'" + " > " + path + "/" + sample+"_peakCalls.bed")
-			print("bedtools intersect -a " + path + "/" + Cancer_type + "*txt.sorted -b " + path + "/" + sample + "* -f 1.0 -c -wa -sorted " + " | awk '{FS=OFS=" + r'"\t"' + ";if($5==2){print $1,$2,$3,$4}}'" + " > " + path + "/" + sample+"_peakCalls.bed")
+	print(sample)
+	if bioSample_dict[sample] == 1: # 1 technical replicate for a ID
+		os.system("bedtools intersect -a " + path + "/" + sample.split("_")[0].replace('x', '') + "*txt.sorted -b " + path + "/" + sample + "*bed.sorted -f 1.0 -wa > " + path + "/" + sample+"_peakCalls.bed")
+		print("bedtools intersect -a " + path + "/" + sample.split("_")[0].replace('x', '') + "*txt.sorted -b " + path + "/" + sample + "*bed.sorted -f 1.0 -wa > " + path + "/" + sample+"_peakCalls.bed")
+
+	elif bioSample_dict[sample] == 2: # 2 technical replicates for a ID
+		os.system("bedtools intersect -a " + path + "/" + sample.split("_")[0].replace('x', '') + "*txt.sorted -b " + path + "/" + sample + "*bed.sorted -f 1.0 -c -wa" + " | awk '{FS=OFS=" + r'"\t"' + ";if($5>1){print $1,$2,$3,$4}}'" + " > " + path + "/" + sample+"_peakCalls.bed")
+		print("bedtools intersect -a " + path + "/" + sample.split("_")[0].replace('x', '') + "*txt.sorted -b " + path + "/" + sample + "*bed.sorted -f 1.0 -c -wa" + " | awk '{FS=OFS=" + r'"\t"' + ";if($5>1){print $1,$2,$3,$4}}'" + " > " + path + "/" + sample+"_peakCalls.bed")
+
 
 # ======================================= Main =======================================
-
 # call peakOverlap() function
-print(Cancer_types[num])
-peakOverlap(Cancer_type=Cancer_types[num], path=Sample_PeakCalls_path)
+print(sorted(list(set(l1)))[num-1]) # set and dict in python don't have order!
+peakOverlap(sample = sorted(list(set(l1)))[num-1], path=Sample_PeakCalls_path)
