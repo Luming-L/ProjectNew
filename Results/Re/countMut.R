6391533a61975a7b55db5011cdfbd18ba52bfe4b
Luming-L <55735395+Luming-L@users.noreply.github.com>
2020-06-27 16:13:43 +0100

    add numOverlaps

--- Results/Re/countMut.R
+++ Results/Re/countMut.R
@@ -1,7 +1,8 @@
 library(stringr)
+library(regioneR)
 
 # read the "TCGA_identifier_mapping.txt (5 columns, the delimiter is "\t")"
-table <- readr::read_tsv("https://api.gdc.cancer.gov/data/7a3d7067-09d6-4acf-82c8-a1a81febf72c")
+table <- readr::read_tsv("/exports/eddie/scratch/s1949868/TCGA_identifier_mapping")
 
 # load snv files in RDS format
 mutect2_snv <- readRDS(file = "/exports/eddie/scratch/s1949868/SNPsAndSmallINDELs/mutect2_snv.rds")
@@ -15,10 +16,15 @@ idx <- grep(ID,stringr::str_extract(table$Case_ID,"[:alnum:]{4}-[:alnum:]{2}-[:a
 cancerType <- unique(substr(table[idx,]$bam_prefix,1,4))
 
 # get the number of somatic mutations for a sample
-num <- nrow(mutect2_snv[mutect2_snv$Sample_ID==ID,])
+numMut <- nrow(mutect2_snv[mutect2_snv$Sample_ID==ID,])
+
+# get the number of overlaps between peaks and mutations for a sample
+mutations <- toGRanges(mutect2_snv[mutect2_snv$Sample_ID==ID,c(3,4,5)])
+peaks <- toGRanges(paste0("/exports/eddie/scratch/s1949868/PRDM9BoundPeaks_410_Case_ID/",ID,"_PRDM9_bound_peaks.bed"))
+numOverlaps <- numOverlaps(A=peaks, B=mutations, count.once=TRUE)
 
 # write the results
-write.table(data.frame(ID,cancerType,num),
+write.table(data.frame(ID,cancerType,numMut,numOverlaps),
 	file="/exports/eddie/scratch/s1949868/SNPsAndSmallINDELs/MutNumber.txt",
 	sep = "\t",
 	append=TRUE,row.names = FALSE,col.names = FALSE,
--- Results/Re/countMut_batch.sh
+++ Results/Re/countMut_batch.sh
@@ -2,7 +2,7 @@
 # Grid Engine options (lines prefixed with #$)
 #$ -cwd -V                  
 #$ -l h_rt=00:05:00
-#$ -l h_vmem=4G
+#$ -l h_vmem=8G
 
 #$ -t 1-404
 
@@ -10,7 +10,7 @@
 . /etc/profile.d/modules.sh
 module load igmm/apps/R/3.6.3
 
-# echo -e "sampleID\tcancerType\tMutNumber" > MutNumber.txt
+# echo -e "sampleID\tcancerType\tMutNumber\tOverlapNumber" > MutNumber.txt
 
 # 
 for file in $(ls /exports/eddie/scratch/s1949868/PRDM9BoundPeaks_410_Case_ID/* | head -n $SGE_TASK_ID | tail -n1); do
