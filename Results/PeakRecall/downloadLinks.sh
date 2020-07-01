bd8d45cdb45ae47d8220bca157813bea570503af
Luming-L <55735395+Luming-L@users.noreply.github.com>
2020-07-01 20:21:18 +0100

    for downloading BigWig files

---
+++ Results/PeakRecall/downloadLinks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -cwd -V                  
+#$ -l h_rt=00:30:00
+#$ -l h_vmem=8G
+ 
+#$ -t 1-23
+
+for link in $(cat /home/s1949868/BedGraph/bigWigLinks.txt | head -n $SGE_TASK_ID | tail -n1); do 
+	wget $link
+done
